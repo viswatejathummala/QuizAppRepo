1cd8f13ccccbaa6bcc1caa23a3a47507be7d321d
Viswateja Thummala <Viswateja.Thummala@cyient.com>
2025-02-28 14:05:09 +0530

    Dashboard Page navigation

--- QuizAppWeb/Controllers/AdminController.cs
+++ QuizAppWeb/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ namespace QuizAppWeb.Controllers
 {
     public class AdminController : Controller
     {
+        public IActionResult Dashboard() => View();
         public IActionResult Index()
         {
             return View();
